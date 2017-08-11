d8bb51cccb50d0ec96dda860d98e28bc284d6201
= <garnaat.ben@gmail.com>
2017-08-10 17:30:45 -0700

    update urls with next_page for logout

--- eatup/urls.py
+++ eatup/urls.py
@@ -20,5 +20,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'accounts/login$', auth_views.login, name='login'),
-    url(r'accounts/logout$', auth_views.logout, name='logout'),
+    url(r'accounts/logout$', auth_views.logout, {'next_page': 'home'}, name='logout'),
 ]
